ca20fc86fa91d11922367eb013bd0f3cca42fea0
KoMMag <kostorv.muhamed@yandex.ru>
2023-10-22 19:51:23 +0300

    Усложненная версия задачи

--- Sprint3/ViewController.swift
+++ Sprint3/ViewController.swift
@@ -8,10 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    
+    @IBOutlet weak var schetchic: UILabel!
+    var nachZnachenie = 0
+    
+    
+    
+    @IBAction func knopkaScetchika(_ sender: Any) {
+        nachZnachenie += 1
+        schetchic.text = "\(nachZnachenie)"
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
