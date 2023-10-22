916c82be8475263d8c7541ce75da6611687ae97e
KoMMag <kostorv.muhamed@yandex.ru>
2023-10-22 19:55:35 +0300

    Конечная версия

--- Sprint3/ViewController.swift
+++ Sprint3/ViewController.swift
@@ -16,12 +16,39 @@ class ViewController: UIViewController {
     
     
     
-    @IBAction func knopkaScetchika(_ sender: Any) {
+    @IBOutlet weak var istoria: UITextView!
+    
+    @IBAction func schetchikPlus(_ sender: Any) {
         nachZnachenie += 1
         schetchic.text = "\(nachZnachenie)"
+        izmeneniaIstoria("Значение увеличено на 1", textView: istoria)
+    }
+      
+    @IBAction func obnulenie(_ sender: Any) {
+        nachZnachenie = 0
+        schetchic.text = "\(nachZnachenie)"
+        izmeneniaIstoria("Значение изменено на 0", textView: istoria)
     }
     
     
+    @IBAction func schetchicMinus(_ sender: Any) {
+        if nachZnachenie > 0 {
+            nachZnachenie -= 1
+            schetchic.text = "\(nachZnachenie)"
+            izmeneniaIstoria("Значение уменьшено на 1", textView: istoria)
+        } else {
+            izmeneniaIstoria("Попытка уменьшить значение счётчика ниже 0", textView: istoria)
+        }
+    }
+    
+    func izmeneniaIstoria(_ message:String, textView: UITextView) {
+        let data = DateFormatter()
+        data.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        let vremya = data.string(from: Date())
+        
+        let vuvodVText = "[\(vremya)]: \(message)\n"
+        textView.text.append(vuvodVText)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
